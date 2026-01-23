23bf27ae5e3932a75c4f78131f4ae1c0dfef5f64
“Ian.Chica” <ian.chica@urv.cat>
2026-01-23 09:20:41 +0100

    hola

--- Incorporacio_PI/backend/server.js
+++ Incorporacio_PI/backend/server.js
@@ -2,60 +2,43 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const http = require('http'); // <--- 1. IMPORTAR HTTP
-const { Server } = require("socket.io"); // <--- 2. IMPORTAR SOCKET.IO
 const { connectDB } = require('./db');
 const { runSeed } = require('./utils/seeder');
 
+
+// Importar rutes
 const authRoutes = require('./routes/authRoutes');
 const studentRoutes = require('./routes/studentRoutes');
 const uploadRoutes = require('./routes/uploadRoutes');
 const miscRoutes = require('./routes/miscRoutes');
-const statsRoutes = require('./routes/statsRoutes');
+const unityRoutes = require('./routes/unityRoutes');
+
 
 const app = express();
 const PORT = 3001;
 
-// <--- 3. CREAR EL SERVIDOR HTTP Y CONFIGURAR SOCKET.IO --->
-const server = http.createServer(app); 
-const io = new Server(server, {
-    cors: {
-        origin: [
-            "http://localhost:5173", 
-            "http://127.0.0.1:5173",
-            "http://localhost:3000" // Si usas otro puerto a veces
-        ],
-        methods: ["GET", "POST"]
-    }
-});
 
-// <--- 4. HACER 'io' ACCESIBLE GLOBALMENTE (TRUCO CLAVE) --->
-// Esto permite usar global.io.emit(...) en logger.js sin importar nada
-global.io = io; 
-
-io.on('connection', (socket) => {
-    console.log('Cliente conectado al socket:', socket.id);
-});
-
+// MIDDLEWARES
 app.use(cors());
 app.use(express.json());
-
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-app.use('/api/login', authRoutes);       
-app.use('/api/students', studentRoutes); 
-app.use('/api/upload', uploadRoutes);    
-app.use('/api/stats', statsRoutes);      
-app.use('/api', miscRoutes);             
 
-// START SERVER (Nota: usamos server.listen en vez de app.listen)
+// RUTES
+app.use('/api/login', authRoutes);
+app.use('/api/students', studentRoutes);
+app.use('/api/upload', uploadRoutes);
+app.use('/api/unity', unityRoutes);
+app.use('/api', miscRoutes); // Agafa /logs, /centros i /analyze
+
+
+// START SERVER
 connectDB().then(async () => {
+    // Executar llavor si cal
     await runSeed();
 
-    // <--- 5. CAMBIAR app.listen POR server.listen --->
-    server.listen(PORT, () => {
-        console.log(`Server corrent en http://localhost:${PORT}`);
+
+    app.listen(PORT, () => {
+        console.log(`🚀 Server running on http://localhost:${PORT}`);
     });
-}).catch((err) => {
-    console.error("Error crític iniciant el servidor:", err);
-});
+}).catch(console.error);
