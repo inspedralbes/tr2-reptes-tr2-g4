af51a13265bc9b640f99254385d2ac945efe56bd
a24kimgalgal <a24kimgalgal@inspedralbes.cat>
2026-01-26 03:14:29 +0100

    multer.js modificat

--- Incorporacio_PI/backend/config/multer.js
+++ Incorporacio_PI/backend/config/multer.js
@@ -18,10 +18,11 @@ const upload = multer({
     fileFilter: (req, file, cb) => {
         const allowedTypes = [
             'application/pdf',
-            'application/vnd.openxmlformats-officedocument.wordprocessingml.document', // docx
-            'application/msword', // doc
-            'application/vnd.oasis.opendocument.text' // odt
+            'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 
+            'application/msword', 
+            'application/vnd.oasis.opendocument.text' 
         ];
+        
         if (allowedTypes.includes(file.mimetype)) {
             cb(null, true);
         } else {
